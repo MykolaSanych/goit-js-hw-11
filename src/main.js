cb9340f37bb3ec31e30f3507b1542b37755f921c
MykolaSanych <ivanushkatt@gmail.com>
2024-05-16 15:06:07 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ const inputSearch = document.querySelector('.input-search');
 const KEY = '43875376-ffcf8bec5b4985f5e1efc350d';
 
 document.querySelector('.input').addEventListener('submit', submitInput);
-function submitInput() {
+function submitInput(event) {
+  event.preventdefault();
   pictureRequest(KEY, inputSearch.value).then(images => {
     const arr = images.hits;
     if (arr.length > 0) {
